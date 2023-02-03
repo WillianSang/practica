63e30f8b5b353b84b781392701b389129f7de2d6
WillianSang <willian.sang.g@gamil.com>
2023-02-03 14:39:17 -0500

    operacion suma

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function saludo() {
 function name() {
   console.log("hola mi name is tal")
 }
+
+const num1 = 2 
+const num2 = 3
+
+const add = (int1, int2) => num1 + num2 
+
+console.log(add(num1, num2)
+)
